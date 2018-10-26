de5575f18631103ff0f86cefe2803ca9db784fc1
holenet <holenet6147@naver.com>
2018-10-26 09:56:08 +0900

    basic chatting log test

--- app/src/main/java/com/cid/bot/ChatActivity.kt
+++ app/src/main/java/com/cid/bot/ChatActivity.kt
@@ -2,11 +2,17 @@ package com.cid.bot
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import android.support.v7.widget.CardView
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.*
+import android.widget.LinearLayout
+import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_chat.*
 
 class ChatActivity : AppCompatActivity() {
     companion object {
@@ -17,6 +23,37 @@ class ChatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+
+        // TODO: replace this message list to the real fetched list
+        val messages = listOf(
+                Message("muBot", "user", "111"),
+                Message("muBot", "user","222"),
+                Message("user", "muBot", "333"),
+                Message("muBot", "user","test cases all right"),
+                Message("user", "muBot", "Hello?"),
+                Message("muBot", "user", "111"),
+                Message("muBot", "user","222"),
+                Message("user", "muBot", "333"),
+                Message("muBot", "user","test cases all right"),
+                Message("user", "muBot", "Hello?"),
+                Message("muBot", "user", "111"),
+                Message("muBot", "user","222"),
+                Message("user", "muBot", "333"),
+                Message("muBot", "user","test cases all right"),
+                Message("user", "muBot", "Hello?"),
+                Message("muBot", "user", "111"),
+                Message("muBot", "user","222"),
+                Message("user", "muBot", "333"),
+                Message("muBot", "user","test cases all right"),
+                Message("user", "muBot", "Hello?"),
+                Message("muBot", "user", "111"),
+                Message("muBot", "user","222"),
+                Message("user", "muBot", "333"),
+                Message("muBot", "user","test cases all right"),
+                Message("user", "muBot", "Hello?")
+        )
+        rVmessages.layoutManager = LinearLayoutManager(applicationContext)
+        rVmessages.adapter = MessageAdapter(messages)
 
         requestSignIn()
     }
@@ -76,5 +113,37 @@ class ChatActivity : AppCompatActivity() {
             else -> return super.onOptionsItemSelected(item)
         }
         return true
+    }
+
+    class MessageAdapter(val messages: List<Message>) : RecyclerView.Adapter<MessageAdapter.ViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.item_message, parent, false)
+            return ViewHolder(view)
+        }
+
+        override fun getItemCount() = messages.size
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            val m = messages[position]
+            with (holder) {
+                tVtext.text = m.text
+                if (m.receiver == "muBot") {
+                    cVmessage.layoutParams = (cVmessage.layoutParams as LinearLayout.LayoutParams).apply {
+                        gravity = Gravity.END
+                    }
+                    cVmessage.setCardBackgroundColor(Color.rgb( 255, 255, 150))
+                } else {
+                    cVmessage.layoutParams = (cVmessage.layoutParams as LinearLayout.LayoutParams).apply {
+                        gravity = Gravity.START
+                    }
+                    cVmessage.setCardBackgroundColor(Color.WHITE)
+                }
+            }
+        }
+
+        class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+            val tVtext = view.findViewById<TextView>(R.id.tVtext)
+            val cVmessage = view.findViewById<CardView>(R.id.cVmessage)
+        }
     }
 }
