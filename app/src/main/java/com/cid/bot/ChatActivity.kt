b36a46745dfdbfc7a1f4705382d5cba1b43b6c95
holenet <holenet6147@naver.com>
2018-11-13 02:34:19 +0900

    chat activity databinding

--- app/src/main/java/com/cid/bot/ChatActivity.kt
+++ app/src/main/java/com/cid/bot/ChatActivity.kt
@@ -1,18 +1,19 @@
 package com.cid.bot
 
 import android.app.Activity
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.content.*
-import android.graphics.Color
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.content.LocalBroadcastManager
-import android.support.v7.widget.CardView
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.*
-import android.widget.LinearLayout
-import android.widget.TextView
 import android.widget.Toast
+import com.cid.bot.databinding.ActivityChatBinding
+import com.cid.bot.databinding.ItemMessageBinding
 import io.reactivex.disposables.Disposable
 import kotlinx.android.synthetic.main.activity_chat.*
 
@@ -21,6 +22,9 @@ class ChatActivity : AppCompatActivity() {
         const val REQUEST_SIGN_IN = 101
         const val REQUEST_PROFILE = 102
     }
+
+    private lateinit var binding: ActivityChatBinding
+    private val messageAdapter = MessageAdapter(mutableListOf())
 
     private val messagingReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent) {
@@ -31,10 +35,18 @@ class ChatActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_chat)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_chat)
+        val viewModel = ViewModelProviders.of(this).get(ChatViewModel::class.java)
+        binding.viewModel = viewModel
+        with (binding) {
+            executePendingBindings()
 
-        rVmessages.layoutManager = LinearLayoutManager(applicationContext)
-        rVmessages.adapter = MessageAdapter(mutableListOf())
+            rVmessages.layoutManager = LinearLayoutManager(applicationContext)
+            rVmessages.adapter = messageAdapter
+        }
+        viewModel.messages.observe(this, Observer { messages ->
+            messages?.let{ messageAdapter.messages = it.sortedBy(Message::created).toMutableList() }
+        })
 
         bTsend.setOnClickListener { trySendMessage() }
         eTtext.setOnKeyListener { v, keyCode, event ->
@@ -183,33 +195,22 @@ class ChatActivity : AppCompatActivity() {
         }
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-            val view = LayoutInflater.from(parent.context).inflate(R.layout.item_message, parent, false)
-            return ViewHolder(view)
+            val inflater = LayoutInflater.from(parent.context)
+            val binding = ItemMessageBinding.inflate(inflater, parent, false)
+            return ViewHolder(binding)
         }
 
         override fun getItemCount() = messages.size
 
         override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-            val m = messages[position]
-            with (holder) {
-                tVtext.text = m.text
-                if (m.receiver == null) {
-                    cVmessage.layoutParams = (cVmessage.layoutParams as LinearLayout.LayoutParams).apply {
-                        gravity = Gravity.END
-                    }
-                    cVmessage.setCardBackgroundColor(Color.rgb( 255, 255, 150))
-                } else {
-                    cVmessage.layoutParams = (cVmessage.layoutParams as LinearLayout.LayoutParams).apply {
-                        gravity = Gravity.START
-                    }
-                    cVmessage.setCardBackgroundColor(Color.WHITE)
-                }
-            }
+            holder.bind(messages[position])
         }
 
-        class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-            val tVtext = view.findViewById<TextView>(R.id.tVtext)
-            val cVmessage = view.findViewById<CardView>(R.id.cVmessage)
+        class ViewHolder(private val binding: ItemMessageBinding) : RecyclerView.ViewHolder(binding.root) {
+            fun bind(message: Message) {
+                binding.message = message
+                binding.executePendingBindings()
+            }
         }
     }
 
--- app/src/main/java/com/cid/bot/view_models.kt
+++ app/src/main/java/com/cid/bot/view_models.kt
@@ -1,20 +1,44 @@
 package com.cid.bot
 
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import android.databinding.ObservableField
 
-
 class ProfileViewModel : ViewModel() {
-    val repoModel = Repository()
+    val repo = Repository()
     val text = ObservableField<String>()
     val isLoading = ObservableField<Boolean>()
 
+    init {
+        refresh()
+    }
+
     fun refresh() {
         isLoading.set(true)
-        repoModel.refreshData(object : OnDataReadyCallback {
+        repo.refreshData(object : OnDataReadyCallback {
             override fun onDataReady(data: String) {
                 isLoading.set(false)
                 text.set(data)
+            }
+        })
+    }
+}
+
+class ChatViewModel : ViewModel() {
+    val repo = Repository()
+    val messages = MutableLiveData<List<Message>>()
+    val isLoading = ObservableField<Boolean>()
+
+    init {
+        loadMessages()
+    }
+
+    fun loadMessages() {
+        isLoading.set(true)
+        repo.getMessages(object : OnMessageReadyCallback {
+            override fun onMessageReady(data: List<Message>) {
+                isLoading.set(false)
+                messages.value = data
             }
         })
     }
