209662f9f501d86cb7afe97668e4cd7c19d4b0ca
holenet <holenet6147@naver.com>
2018-11-19 02:10:33 +0900

    delete unnecessary code

--- app/src/main/java/com/cid/bot/base_activities.kt
+++ app/src/main/java/com/cid/bot/base_activities.kt
@@ -1,6 +1,5 @@
 package com.cid.bot
 
-import androidx.appcompat.app.AppCompatActivity
 import dagger.android.support.DaggerAppCompatActivity
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.Observable
@@ -8,31 +7,6 @@ import retrofit2.Response
 import javax.inject.Inject
 
 abstract class BaseDaggerActivity : DaggerAppCompatActivity() {
-    @Inject lateinit var net: NetworkManager
-    private val compositeDisposable = CompositeDisposable()
-
-    fun <T> register(observable: Observable<Response<T>>,
-                     onSuccess: (T) -> Unit,
-                     onError: (Map<String, String>) -> Unit,
-                     onFinish: () -> Unit = {}
-    ) {
-        compositeDisposable += net.call(observable, {
-            onSuccess(it)
-        }, {
-            onError(it)
-        }, {
-            onFinish()
-        })
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        if (!compositeDisposable.isDisposed)
-            compositeDisposable.dispose()
-    }
-}
-
-abstract class BaseActivity: AppCompatActivity() {
     @Inject lateinit var net: NetworkManager
     private val compositeDisposable = CompositeDisposable()
 
