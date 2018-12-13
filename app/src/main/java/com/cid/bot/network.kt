92c4db86ae10f25d54c5f9dc7fe36a6feedaff7a
holenet <holenet6147@naver.com>
2018-12-13 11:52:43 +0900

    version setting

--- app/src/main/java/com/cid/bot/network.kt
+++ app/src/main/java/com/cid/bot/network.kt
@@ -102,7 +102,6 @@ fun <T> Observable<Response<T>>.toHResult(): Observable<HResult<T>> {
 }
 
 const val BASE_URL = "http://52.78.179.149"
-//    const val BASE_URL = "http://10.0.2.2:8000" /* development environment */
 
 @Singleton
 class NetworkManager @Inject constructor(private val context: Context) {
